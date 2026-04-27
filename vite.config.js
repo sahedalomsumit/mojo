dcfa6b43d206c8d1a7101f0cdfc1cb08718f4970
Sahed Alom Sumit <sahedalomsumit@gmail.com>
2026-04-27 12:29:40 +0300

    fix: update base path in vite config to root directory

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lemillion/',
+  base: '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
